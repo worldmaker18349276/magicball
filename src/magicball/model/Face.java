fb41cb0dbae9f1f430601cc468046df51724b6ca
worldmaker <worldmaker18349276@gmail.com>
2014-09-07 20:28:44 +0800

    add Face

---
+++ src/magicball/model/Face.java
@@ -0,0 +1,17 @@
+package magicball.model;
+
+
+public abstract class Face
+{
+	public boolean equals( Object face ) {
+		if ( face instanceof Face )
+			return equals((Face) face);
+		else
+			return false;
+	}
+
+	public abstract boolean equals( Face face );
+	public abstract Face clone();
+
+	public abstract void apply( Displacement dis );
+}
--- src/magicball/model/RegionBuilder.java
+++ src/magicball/model/RegionBuilder.java
@@ -9,4 +9,5 @@ public interface RegionBuilder
 	public Region createComplementRegion( Region reg1, Region reg2 );
 	public Region createUniverseRegion();
 	public Region createEmptyRegion();
+	public Region createRegionByFace( Face face, int side );
 }
