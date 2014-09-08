3a2f1b0e4f6467e910f948ac0dc39a9f3a262969
worldmaker <worldmaker18349276@gmail.com>
2014-09-08 19:18:19 +0800

    add DisplacementBuilder

---
+++ src/magicball/model/DisplacementBuilder.java
@@ -0,0 +1,12 @@
+package magicball.model;
+
+
+// geometry abstraction layer
+public interface DisplacementBuilder
+{
+	public Displacement createIdentityDisplacement();
+	public Displacement createCompositeDisplacement( Displacement... dis );
+	public Displacement createInverseDisplacement( Displacement dis );
+	public Displacement createRotation( Number [] axis, Number deg );
+	public Displacement createShift( Number [] vec );
+}
