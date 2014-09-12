a13debc720cd8bd2e00a426b3f274aa7d5cbf30c
worldmaker <worldmaker18349276@gmail.com>
2014-09-12 12:23:04 +0800

    modify structure of Solid

--- src/magicball/model/puzzle/Solid.java
+++ src/magicball/model/puzzle/Solid.java
@@ -1,7 +1,22 @@
 package magicball.model.puzzle;
 
+import magicball.model.geometry.*;
+
 
 // definition: a geometric object that occupy a specific region and is rigid.
-public abstract class Solid
+public class Solid < R extends Region >
 {
+	final private R region;
+
+	public Solid( R reg ) {
+		this.region = reg;
+	}
+
+	public Solid<R> clone() {
+		return new Solid<R>(getOccupiedRegion());
+	}
+
+	final public R getOccupiedRegion() {
+		return this.region;
+	}
 }
