590f497282655c0d67891d7e42580a7f6b9fe408
worldmaker <worldmaker18349276@gmail.com>
2015-02-14 23:56:32 +0800

    add reg func exp

---
+++ src/magicball/model/geometry/basic/RegionFuncExpression.java
@@ -0,0 +1,18 @@
+package magicball.model.geometry.basic;
+
+import magicball.model.geometry.*;
+import magicball.model.math.*;
+
+
+public class RegionFuncExpression extends Region
+{
+	final private Function<Number[],Boolean> function;
+
+	public RegionFuncExpression( Function<Number[],Boolean> function ) {
+		this.function = function;
+	}
+
+	final public Function<Number[],Boolean> getFunction() {
+		return this.function;
+	}
+}
