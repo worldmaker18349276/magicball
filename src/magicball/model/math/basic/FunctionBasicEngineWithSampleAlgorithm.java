38cc7e8a6bf6bc27e58bc7f8409614f5d6d20d75
worldmaker <worldmaker18349276@gmail.com>
2015-02-14 23:45:17 +0800

    add func sample algo

---
+++ src/magicball/model/math/basic/FunctionBasicEngineWithSampleAlgorithm.java
@@ -0,0 +1,65 @@
+package magicball.model.math.basic;
+
+import java.util.stream.*;
+
+import magicball.model.*;
+import magicball.model.math.*;
+
+public class FunctionBasicEngineWithSampleAlgorithm < E > extends FunctionBasicEngine
+{
+	protected java.util.Set<E> samples;
+
+	public FunctionBasicEngineWithSampleAlgorithm( java.util.Set<E> sam ) {
+		this.samples = sam;
+	}
+
+	@Override
+	public FunctionBasicEngineWithSampleAlgorithm<E> clone() {
+		return new FunctionBasicEngineWithSampleAlgorithm<E>(this.samples);
+	}
+
+	@SuppressWarnings({"unchecked"})
+	private < O > Function<E,O> castToE( Function<?,O> func ) {
+		try {
+			return (Function<E,O>) func;
+		} catch ( ClassCastException e ) {
+			throw new UnsupportedExpressionException(func.getClass());
+		}
+	}
+
+
+	@Override
+	public < I, O > Function<O,I> invert( Function<I,O> func_ ) {
+		throw new UnsupportedAlgorithmException();
+	}
+
+	@Override
+	public < I, O > boolean equals( Function<I,O> func1_, Function<I,O> func2_ ) {
+		Function<E,O> func1 = this.castToE(func1_);
+		Function<E,O> func2 = this.castToE(func2_);
+		return samples.stream()
+			.allMatch(e -> applies(func1,e).equals(applies(func2,e)));
+	}
+
+	@Override
+	public < I > boolean isAlwaysTrue( Function<I,Boolean> func_ ) {
+		Function<E,Boolean> func = this.castToE(func_);
+		return samples.stream()
+			.allMatch(e -> applies(func,e));
+	}
+
+	@Override
+	public < I > boolean isAlwaysFalse( Function<I,Boolean> func_ ) {
+		Function<E,Boolean> func = this.castToE(func_);
+		return samples.stream()
+			.noneMatch(e -> applies(func,e));
+	}
+
+	@Override
+	public < I > boolean implies( Function<I,Boolean> func1_, Function<I,Boolean> func2_ ) {
+		Function<E,Boolean> func1 = this.castToE(func1_);
+		Function<E,Boolean> func2 = this.castToE(func2_);
+		return samples.stream()
+			.allMatch(e -> applies(func2,e)||!applies(func1,e));
+	}
+}
